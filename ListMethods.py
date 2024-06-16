9509d458f8fbf0864c15deb33e9a3ded80d49ba2
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-16 10:28:17 +0500

    55

--- ListMethods.py
+++ ListMethods.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]  # add a new number to the end of the list
-# numbers.append(6)
-numbers.insert(0, 6)  # add a new number to the beginning of the list
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6) # add a new number to the end of the list
+numbers.insert(0, -1)  # add a new number to the beginning of the list
+numbers.remove(3)  # remove the first instance of 3 from the list
 print(numbers)
