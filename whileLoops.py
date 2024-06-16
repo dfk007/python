6413cacbca86af84e034ffca48d862a917cbd82b
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-16 09:42:22 +0500

    55

--- whileLoops.py
+++ whileLoops.py
@@ -1,6 +1,6 @@
 """ WHILE LOOPS """
 
 i = 1
-while i <= 5:  # Long as condition is true, while will run
-    print(i)
+while i <= 1_0:  # Long as condition is true, while will run
+    print(i * '*')
     i = i + 1
