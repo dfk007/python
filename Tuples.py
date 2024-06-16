a4e6586e033be3de25840a862cb6c374d17e7107
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-16 11:01:53 +0500

    55

---
+++ Tuples.py
@@ -0,0 +1,6 @@
+# TUPLES - IMMUTABLE
+
+numbers = (1, 2, 3) # TUPLE
+numbers[0] = 100 # ERROR
+# print(numbers[0])
+
---
+++ rangeFunction.py
@@ -0,0 +1,6 @@
+# THE RANGE() FUNCTION
+
+numbers = range(5, 10)  # an object that can store a sequence of integers from 0 to 4
+
+for number in range(5, 10):
+    print(number)
