e2c002476595553626affdbcb3d985fee776191a
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-16 11:07:37 +0500

    Tuples

--- Tuples.py
+++ Tuples.py
@@ -1,6 +1,9 @@
 # TUPLES - IMMUTABLE
 
-numbers = (1, 2, 3) # TUPLE
-numbers[0] = 100 # ERROR
+numbers = (1, 2, 3, 1) # TUPLE
+# numbers[0] = 100 # ERROR
 # print(numbers[0])
 
+# numbers.count(3)
+print(numbers.count(1)) # number of occurences
+print(numbers.index(3)) # index of the first occurence
